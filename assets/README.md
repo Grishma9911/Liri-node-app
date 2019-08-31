c49c9fbdb576726bb1728d28f75803bbd5e91cbe
grishma9911 <patelgrishma9911@gmail.com>
2019-08-31 00:31:01 -0400

    liri node app

--- assets/README.md
+++ assets/README.md
@@ -2,24 +2,50 @@
 
 LIRI will be a command line node app that takes in parameters and gives you back data.
 
+### What it does ?
+
 1. `node liri.js concert-this <artist/band name here>`
 
    * This will search the Bands in Town Artist Events for an artist and render the following information about each event to the terminal:
 
      * Name of the venue
-
      * Venue location
-
-     * Date of the Event (use moment to format this as "MM/DD/YYYY")
+     * Date of the Event (format as "MM/DD/YYYY")
 
 2. `node liri.js spotify-this-song '<song name here>'`
 
    * This will show the following information about the song in your terminal/bash window
 
      * Artist(s)
-
      * The song's name
-
      * A preview link of the song from Spotify
-
      * The album that the song is from
+
+3. `node liri.js movie-this '<movie name here>'`
+
+   * This will output the following information to your terminal/bash window:
+   
+       * Title of the movie.
+       * Year the movie came out.
+       * IMDB Rating of the movie.
+       * Rotten Tomatoes Rating of the movie.
+       * Country where the movie was produced.
+       * Language of the movie.
+       * Plot of the movie.
+       * Actors in the movie.
+
+4. `node liri.js do-what-it-says`
+
+   * Using the `fs` Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
+
+     * It should run `spotify-this-song` for "I Want it That Way," as follows the text in `random.txt`.
+
+### How users can get started with the project?
+
+* Anyone can pull the repo or take code snipet. 
+    * Then install necessary npm packages.
+    * Run it in bash, and you good to go.
+
+### Where users can get help with your project?
+
+* Liri node app is like siri in iphone, it does what it is said. 
--- assets/liri2.js
+++ assets/liri2.js
@@ -1,8 +1,7 @@
 require("dotenv").config();
 
-
 var command = process.argv[2];
-var search = process.argv[3];
+var search = process.argv.slice(3).join("");
 var keys = require('./keys.js');
 
 // spotify-this-songs:
@@ -29,6 +28,7 @@ var getMeSpotify = function (songName) {
 
             for (var i = 0; i < songs.length; i++) {
                 //   console.log(i);
+                console.log("-----------*SPOTIFY_THIS_SONG*----------------");
                 console.log("artist(s): " + songs[i].artists);
                 console.log("song name: " + songs[i].name);
                 console.log("preview song: " + songs[i].preview_url);
@@ -49,6 +49,7 @@ function movies() {
     axios.get("http://www.omdbapi.com/?t=" + search + "&y=&plot=short&apikey=trilogy").then(
         function (response) {
             // Then we print out the imdbRating
+            console.log("----------------*MOVIE_THIS*-------------------");
             console.log("Title: " + response.data.Title);
             console.log("Rating: " + response.data.imdbRating);
             console.log("Year: " + response.data.Year);
@@ -61,42 +62,7 @@ function movies() {
         }
     )
 }
-movies();
-
-// what does it says:
-
-// function doIt() {
-
-//     var fs = require("fs");
-//     fs.readFile("random.txt", "utf8", function (err) {
-//         if (err) {
-//             return console.log(err);
-//         } else{
-//             // Otherwise, it will print: "movies.txt was updated!"
-//             console.log("movies.txt was updated!");
-//         }
-//     });
-//     doIt();
-// };
-
-// Serach & command function:
-switch (command) {
-    case "concert-this":
-        concertThis(search)
-        break;
-
-    case "spotify-this-song":
-        getMeSpotify(search)
-        break;
-
-    case "do-what-it-says":
-        doIt(search)
-        break;
-
-    case "movie-this":
-        movies(search);
-        break;
-}
+movies("Titanic");
 
 // concert-this:
 var axios = require("axios");
@@ -105,11 +71,69 @@ function concertThis() {
     axios.get("https://rest.bandsintown.com/artists/" + search + "/events?app_id=codingbootcamp").then(
         function (response) {
             // Printout results:
-            console.log("Name of the venue is: " + response.data.venue[3]);
-            console.log("Venue of location is: " + response.data.venue[1,0]);
+            console.log("----------------*CONCERT_THIS*-------------------");
+            console.log("Name of the venue is: " + response.data.venue[0]);
+            console.log("Venue of location is: " + response.data.venue["city"]);
             console.log("Date of the event is: " + moment(result.datetime).format("MM/DD/YY"));
             console.log("-----------------------------------");
+            if (err) {
+                // output err or do something with it
+                console.log('failed to build transaction');
+                console.log(err);
+        
+                // stop here.
+                return;
+            }
+        
         }
     )
 }
-concertThis();
+concertThis("BECK");
+
+// Read file from random.txt:
+
+function random() {
+    var data = "";
+    var fs = require("fs");
+    fs.readFile("random.txt", "utf8", function (err) {
+        if (err) {
+            return console.log(err);
+        } else{
+            var ramdata = data.split(",");
+            liriComSea(ramdata[0], ramdata[1]);
+            console.log("-----------------------------------");
+        }
+    });
+};
+random();
+
+// Serach & command function:
+function liriComSea(command, search){
+
+    switch (command) {
+        case "concert-this":
+            concertThis(search)
+        break;
+        
+    case "spotify-this-song":
+        getMeSpotify(search)
+        break;
+        
+    case "do-what-it-says":
+        random(search)
+        break;
+        
+    case "movie-this":
+        movies(search);
+        break;
+    default:
+        console.log("search for three of these!!!!")
+        console.log("-----------------*COMMAND-SEARCH*------------------");
+    }
+}
+liriComSea();
+
+// Needs to be done:
+
+//  If no song is provided then your program will default to "The Sign" by Ace of Base.
+//  If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
